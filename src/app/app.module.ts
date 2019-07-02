aa19bb6546cb96a355b069439798a36a4acbe282
egonher <egonher@gesplan.es>
2019-07-02 13:23:11 +0100

    añado componente de login y reestructuro la página de acceso público.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CatalogueComponent } from './catalogue/catalogue.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CatalogueComponent
+    CatalogueComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
