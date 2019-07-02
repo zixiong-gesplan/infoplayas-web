8da5002237ac2b3455388da23d1dc6902062a30e
egonher <egonher@gesplan.es>
2019-07-02 16:45:35 +0100

    fix: no cargaba la página

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {AppComponent} from './app.component';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {ProfileComponent} from './profile/profile.component';
 import {AuthGuardService} from './services/auth-guard.service';
 
 const routes: Routes = [
-  {path: '', component: AppComponent},
-  {path: 'tecnicos', component: ProfileComponent, canActivate: [AuthGuardService]},
-  { path: '**', redirectTo: '' }
+    {path: '', redirectTo: '/', pathMatch: 'full'},
+    {path: 'tecnicos', component: ProfileComponent, canActivate: [AuthGuardService]},
+    {path: '**', redirectTo: ''}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
