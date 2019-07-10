9004c657010ad3de8cf040893084dfc894620726
egonher <egonher@gesplan.es>
2019-07-10 13:36:04 +0100

    creando la estructura del profile component, la parte del técnico logueado. Empiezo creando sección para el catálogo.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,19 @@ import {Routes, RouterModule} from '@angular/router';
 import {ProfileComponent} from './profile/profile.component';
 import {AuthGuardService} from './services/auth-guard.service';
 import {HomeComponent} from './home/home.component';
+import {CatalogueComponent} from './profile/catalogue/catalogue.component';
 
 const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
-    {path: 'tecnicos', component: ProfileComponent, canActivate: [AuthGuardService]},
+    {path: 'tecnicos', component: ProfileComponent, children: [
+            {path: '', redirectTo: 'catalogo', pathMatch: 'full'},
+            {path: 'tecnicos', redirectTo: 'catalogo', pathMatch: 'full'},
+            {
+                path: 'catalogo', component: CatalogueComponent, canActivate: [AuthGuardService],
+            }
+        ]
+    },
     {path: '**', redirectTo: '/home'}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,17 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {CatalogueComponent} from './catalogue/catalogue.component';
 import {LoginComponent} from './login/login.component';
 import {ProfileComponent} from './profile/profile.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HomeComponent } from './home/home.component';
+import {CatalogueComponent} from './profile/catalogue/catalogue.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        CatalogueComponent,
         LoginComponent,
+        CatalogueComponent,
         ProfileComponent,
         HomeComponent
     ],
