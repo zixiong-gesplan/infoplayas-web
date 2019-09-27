1a035449c916663a5f7facfe64877cffa93ed4c5
egonher <egonher@gesplan.es>
2019-09-27 11:51:30 +0100

    kjdkfd

--- src/app/profile/map-editor/map-editor.component.ts
+++ src/app/profile/map-editor/map-editor.component.ts
@@ -73,6 +73,8 @@ export class MapEditorComponent implements OnInit {
     dates: Date[];
     es: any;
     private currentUser: Auth;
+    minDate: Date;
+    maxDate: Date;
 
     constructor(private authService: AuthGuardService, private service: EsriRequestService, private fb: FormBuilder,
                 private spinnerService: Ng4LoadingSpinnerService) {
@@ -158,6 +160,20 @@ export class MapEditorComponent implements OnInit {
             today: 'Hoy',
             clear: 'Borrar'
         };
+        // fechas maxima y minima para los calendarios de afluencias
+        let today = new Date();
+        let month = today.getMonth();
+        let year = today.getFullYear();
+        let prevMonth = (month === 0) ? 11 : month - 1;
+        let prevYear = (prevMonth === 11) ? year - 1 : year;
+        let nextMonth = (month === 11) ? 0 : month + 1;
+        let nextYear = (nextMonth === 0) ? year + 1 : year;
+        this.minDate = new Date();
+        this.minDate.setMonth(prevMonth);
+        this.minDate.setFullYear(prevYear);
+        this.maxDate = new Date();
+        this.maxDate.setMonth(nextMonth);
+        this.maxDate.setFullYear(nextYear);
     }
 
     loadRelatedRecords() {
