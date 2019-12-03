4de0da5d860fed6ac6c4e8e3518766a533922b10
egonher <egonher@gesplan.es>
2019-12-03 11:12:47 +0000

    fix: controlar el fade loader

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -9,9 +9,6 @@ import {PopulationService} from '../services/population.service';
 import {AppSettingsService} from '../services/app-settings.service';
 import {AppSetting} from '../models/app-setting';
 
-declare var $: any;
-declare var jQuery: any;
-
 @Component({
     selector: 'app-profile',
     templateUrl: './profile.component.html',
