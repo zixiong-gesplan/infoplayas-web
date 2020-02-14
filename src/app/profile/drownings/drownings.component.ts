9e56ffd9b5db78fffe26dfeffedaae45be97abd1
egonher <egonher@gesplan.es>
2020-02-14 16:56:20 +0000

    para test.

--- src/app/profile/drownings/drownings.component.ts
+++ src/app/profile/drownings/drownings.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MapViewerComponent} from '../map-viewer/map-viewer.component';
+import {DialogService} from 'primeng/api';
+import {MapPickLocationComponent} from '../map-pick-location/map-pick-location.component';
 
 @Component({
-  selector: 'app-drownings',
-  templateUrl: './drownings.component.html',
-  styleUrls: ['./drownings.component.css']
+    selector: 'app-drownings',
+    templateUrl: './drownings.component.html',
+    styleUrls: ['./drownings.component.css']
 })
 export class DrowningsComponent implements OnInit {
 
-  constructor() { }
+    constructor(public dialogService: DialogService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    test() {
+        const ref = this.dialogService.open(MapPickLocationComponent, {
+            data: {
+                id: 'this.beachObjectId',
+                zoom: 12,
+                mapHeight: '69vh'
+            },
+            header: 'titulo cabecera ventana',
+            width: '65%',
+            contentStyle: {'max-height': '78vh', 'overflow': 'auto'}
+        });
+
+        ref.onClose.subscribe((mensaje: string) => {
+            if (mensaje) {
+                console.log(mensaje);
+            }
+        });
+        // this.loadAllData();
+        // this.vacacional = false;
+    }
 
 }
